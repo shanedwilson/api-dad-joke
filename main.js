1e56ba8572e6419eda727f32a17de8ba688331d9
Shane D Wilson <me@shanedwilson.com>
2018-10-28 08:38:23 -0500

    working

---
+++ main.js
@@ -0,0 +1,31 @@
+const dadJokeAPI = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: "https://icanhazdadjoke.com/",
+            headers: {
+                accept: "application/json"
+            }
+        })
+            .done(joke => {
+                resolve(joke);
+            })
+            .fail(error => {
+                reject(error);
+            })
+    })
+   
+};
+
+const getAJoke = () => {
+    dadJokeAPI()
+        .then(joke => {
+            $('#joke').html(`<p>${joke.joke}</p>`)
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+$('button').on('click', (e) => {
+    getAJoke();
+});
